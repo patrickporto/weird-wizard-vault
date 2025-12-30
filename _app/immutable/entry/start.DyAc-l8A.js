f87d84efdf98a28b960fce4c0ae1424cfe4b8507
patrickporto <patrickporto@users.noreply.github.com>
2025-12-30 20:06:14 +0000

    deploy: 0365125c365e02cfd4a992b7fea2250ada72db37

--- _app/immutable/entry/start.DyAc-l8A.js
+++
@@ -1,1 +0,0 @@
-import{l as o,c as r}from"../chunks/DzijH9Fn.js";export{o as load_css,r as start};
---
+++ _app/immutable/entry/start.fL4TREX2.js
@@ -0,0 +1,1 @@
+import{l as o,c as r}from"../chunks/BjVE1IS8.js";export{o as load_css,r as start};
