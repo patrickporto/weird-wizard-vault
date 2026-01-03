dd189c1d62cb4a0b845f98786ca6e2127138da54
patrickporto <patrickporto@users.noreply.github.com>
2026-01-03 00:45:00 +0000

    deploy: 654118826070448843b9228906f6c44cf44ba4c4

--- _app/immutable/chunks/BW9HaG-_.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./xs3qUsLd.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
